8a9343824eff21226233eb00fc690d986081113f
Samira <samira@Samiras-MacBook-Pro.local>
2020-10-20 00:01:00 +0330

    Change fontsize for cards and rename the view template

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#index"
   get '/devices/345/status', to: 'pages#device_status'
-  get 'devices/345/analytics', to: 'pages#analytics'
+  get 'devices/345/analytics', to: 'pages#device-analytics'
 end
